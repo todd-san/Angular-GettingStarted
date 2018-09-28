292096e6d2551c96c2829a483dbf434fb032c32f
tmiller <tmiller@kendausa.com>
2018-09-28 17:08:02 -0400

    auto update with "sudo" on linux, adding some style sheets, trying to build out the functionality now.

--- kontext/src/app/app.module.ts
+++ kontext/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { BaseComponent } from './base/base.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule
